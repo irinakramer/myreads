c6fa9eb7d6e076887fb74ea35ce67bfb00f68f8f
Irina Kramer <irakramer@gmail.com>
2021-02-25 12:41:47 -0800

    refactor: Organized code between Main, Shelf and Book components

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 class SearchPage extends Component {
     render() {
         return (
-            <div className="search-books">
+          <div className="search-books">
             <div className="search-books-bar">
               <Link className="close-search" to="/">Close</Link>
               <div className="search-books-input-wrapper">
