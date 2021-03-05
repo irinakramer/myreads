228f22357541aaf32d5a4ab95e3a4f2a1279435c
Irina Kramer <irakramer@gmail.com>
2021-03-04 23:08:01 -0800

    refactor: Converted Shelf and Book to functional components

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,36 +1,26 @@
-import React, {Component} from 'react';
-import * as BooksAPI from '../BooksAPI'
+import React from 'react';
 
-class Book extends Component {
-    // componentDidMount() {
-    //     console.log(this)
-    // }
-    
-
-    render() {
-        return (
-            <li>
-                <div className="book">
-                    <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail || ""})` }}></div>
+const Book = ({ book, updateBook }) => {
+    return (
+        <li>
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail || ""})` }}></div>
                     <div className="book-shelf-changer">
-                        <select value={this.props.book.shelf || "none"} onChange={(e) => {this.props.updateBook(this.props.book, e.target.value)}}>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
+                        <select value={book.shelf || "none"} onChange={(e) => { updateBook(book, e.target.value) }}>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
                         </select>
                     </div>
-                    </div>
-                    <div className="book-title">{this.props.book.title || "No Title..."}</div>
-                    <div className="book-authors">{this.props.book.authors && this.props.book.authors[0] || "No Author..."}</div>
                 </div>
-            </li>
-
-
-        )
-    }
+                <div className="book-title">{book.title || "No Title..."}</div>
+                <div className="book-authors">{book.authors && book.authors[0] || "No Author..."}</div>
+            </div>
+        </li>
+    )
 }
 
 export default Book;
--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,28 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Book from './Book';
 
-class Shelf extends Component {
-  componentDidMount() {
-    console.log(this)
-  }
-    render() {
-        return (
-            <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">{this.props.name}</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {
-                        this.props.books.map((book, key) => <Book updateBook={this.props.updateBook}  book={book} key={key} />)
-                      }
-                    </ol>
-                  </div>
-                </div>
-              </div>
-
-
-        )
-    }
+const Shelf = (props) => {
+  return (
+    <div>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{props.name} {props.books.length}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {
+              props.books.map((book, key) =>
+                <Book
+                  updateBook={props.updateBook}
+                  book={book}
+                  key={key}
+                />
+              )
+            }
+          </ol>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Shelf;
